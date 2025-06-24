25a4812d91922243225878eeca2b8b8030491486
Nexsosphere-Technologies <nexsosphere.technologies@gmail.com>
2025-06-24 12:22:33 +0100

    Updated package-lock.json

--- src/components/TabBarIcon.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Feather } from '@expo/vector-icons';
-
-export default function TabBarIcon({ name, color, size }) {
-  return <Feather name={name} size={size} color={color} />;
-}
